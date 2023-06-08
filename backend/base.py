aa95b4050ce3dc121dc4c446eaf6b799974c702e
reeceyankey <reeceyankey@gmail.com>
2023-06-08 18:15:07 -0400

    added jwt tokens

--- backend/base.py
+++ backend/base.py
@@ -1,14 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
 from pymongo import MongoClient
 import json
 import pymongo
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
-db = client['recipe_db']
-recipes = db['recipes']
 
-@app.route("/query", methods=['POST'])
-def index():
-    return "Hello"
+app.config["JWT_SECRET_KEY"] = 'secret'
+jwt = JWTManager(app)
+
+# client = MongoClient('localhost', 27017)
+# db = client['article_db']
+# articles = db['articles']
+# users = db['users']
+# comments = db['comments']
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods=['GET'])
+@jwt_required()
+def protected():
+    user = get_jwt_identity()
+    print(user)
+    return jsonify(logged_in_as=user)
+
 
