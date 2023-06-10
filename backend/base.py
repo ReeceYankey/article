57785b45d025a0f5e08080cd466ade64aef70de2
reeceyankey <reeceyankey@gmail.com>
2023-06-10 12:07:52 -0400

    remove unnecessary import

--- backend/base.py
+++ backend/base.py
@@ -4,7 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 import pymongo
 from bson.objectid import ObjectId
-import json
 import time
 from datetime import timedelta
 
