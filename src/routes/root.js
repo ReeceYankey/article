5f8476592e477d114bd4f87747e991c9d0e1fb3f
reeceyankey <reeceyankey@gmail.com>
2023-06-11 20:07:02 -0400

    remove unnecessary comments

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,21 +1,10 @@
 import { useLoaderData } from 'react-router-dom';
 import './root.css';
-import { useState } from 'react';
 import axios from 'axios';
 
-// function 
-//   axios({
-//     method: 'GET',
-//     url: 'ingredientlist',
-//   }).then((response) => {
-//     let res = response.data;
-//     // console.log(res)
-//     setIngredients(res);
-//   })
 export async function loader(){
   const res = await axios.get('/list_articles')
-  console.log(res);
-  console.log(res.data);
+  // console.log(res.data);
   return res.data;
 }
 
